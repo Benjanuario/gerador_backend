df15f5f45feb2b696459719595f45801945bd713
Benjamim Januário Paulino <benjanuariosite@gmail.com>
2026-05-23 11:19:14 +0200

    Update gratisunluckQuinzenalRoutes.js

--- routes/gratisunluckQuinzenalRoutes.js
+++ routes/gratisunluckQuinzenalRoutes.js
@@ -7,5 +7,8 @@ router.post("/gerar-pdf", gratisunluckQuinzenalController.gerarPDF);
 router.post("/consumir-credito", gratisunluckQuinzenalController.consumirCredito);
 router.post("/validar-codigo", gratisunluckQuinzenalController.validarCodigo);
 router.post("/verificar-creditos", gratisunluckQuinzenalController.verificarCreditos);
+router.post("/verificar-download", gratisunluckQuinzenalController.verificarDownload);
+router.post("/marcar-download", gratisunluckQuinzenalController.marcarDownload);
+router.post("/dados-plano", gratisunluckQuinzenalController.getDadosPlano);
 
 module.exports = router;
